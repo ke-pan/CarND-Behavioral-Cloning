582030bf584aece11b803980166d1a7044c5fbfd
ke-pan <ke_pan@yeah.net>
2017-01-25 17:12:30 +0800

    image argumentation

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import csv
 import cv2
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D, Flatten, ELU, Lambda
 import numpy as np
 from random import sample, randint, uniform
 # from sklearn.model_selection import train_test_split
@@ -25,7 +25,13 @@ def pick_image_filename(x, steering):
     return x[i], steering1
 
 def trans_image(image, steering):
-    return image, steering
+    rows, cols, _ = image.shape
+    trans_x = randint(-50, 51)
+    trans_steering = trans_x * 0.004
+    trans_y = randint(-10, 11)
+    trans_M = np.array([[1, 0, trans_x], [0, 1, trans_y]])
+    trans_image = cv2.warpAffine(image, trans_M, (cols, rows))
+    return trans_image, trans_steering
 
 def flip_image(image, steering):
     if randint(0, 1) == 1:
@@ -36,6 +42,11 @@ def argument_brightness(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     hsv[:,:,2] = hsv[:,:,2] * uniform(0.3, 1.3)
 
+def crop(image):
+    return image[30:140,:,:]
+
+def resize(image):
+    return cv2.resize(image,(WIDTH,HEIGHT),interpolation=cv2.INTER_AREA)
 
 def generator(batch_size, data):
     images = np.zeros((batch_size, WIDTH, HEIGHT, CHANNEL))
@@ -57,17 +68,22 @@ def generator(batch_size, data):
         yield images, steerings
 
 def build_model():
+    row, col, ch = HEIGHT, WIDTH, CHANNEL
     model = Sequential()
-    model.add(Convolution2D(16, 5, 5, border_mode='same', input_shape=shape, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), border_mode='same'))
-    model.add(Convolution2D(32, 5, 5, border_mode='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), border_mode='same'))
-    model.add(Convolution2D(64, 5, 5, border_mode='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), border_mode='same'))
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+                     input_shape=(row, col, ch),
+                     output_shape=(row, col, ch)))
+    model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
     model.add(Flatten())
-    # model.add(Dropout(0.2))
-    model.add(Dense(512, activation='relu'))
-    # model.add(Dense(16, activation='relu'))
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(512))
+    model.add(Dropout(.5))
+    model.add(ELU())
     model.add(Dense(1))
     model.summary()
     return model
@@ -84,28 +100,9 @@ with open("driving_log.csv") as f:
     reader = csv.DictReader(f)
     for row in reader:
         log_records.append(row)
-
 print("length of log is:", len(log_records))
-
-# normalize
-# images = np.array(images).astype('float32')
-# images = images / 127.5 - 1.
-# # images = np.array(images)
-#
-#
-# # size of image
-# shape = images[0].shape
-#
-# # train
-#
-# model.compile(loss='mean_squared_error',
-#               optimizer='adam',
-#               metrics=['mean_squared_error'])
-#
-# for i in range(10):
-#     # training set and validation set
-#     train_images, val_images, train_angles, val_angles = train_test_split(images, angles, test_size=0.2)
-#     history = model.fit(train_images, train_angles,
-#                         batch_size=128, nb_epoch=2,
-#                         verbose=1, validation_data=(val_images, val_angles))
-#
+model = build_model()
+model.compile(loss='mse', optimizer='adam')
+history = model.fit_generator(generator, 10000, nb_epoch=3, verbose=1,
+                              alidation_data=generator, nb_val_samples=1000)
+save_model()
